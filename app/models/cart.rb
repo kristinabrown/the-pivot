699bd0758c36b2e6fbdb6e93cf659df5735e39b9
Kristina Brown <kristina.frey.frey@gmail.com>
2015-04-30 09:29:24 -0600

    getting tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,13 +4,13 @@ class Cart
   def initialize(initial_contents)
     @contents = initial_contents || {}
   end
-
+  
   def add_beer(beer, quantity)
     if contents.has_key?(beer.id)
       contents[beer.id][:quantity] += quantity
       contents[beer.id][:beer_price] = beer.price * contents[beer.id][:quantity]
     else
-      contents[beer.id] = {beer_name: beer.name, beer_price: beer.price, quantity: quantity} 
+      contents[beer.id] = {beer_name: beer.name, beer_price: beer.price, quantity: quantity}
     end
   end
 end
