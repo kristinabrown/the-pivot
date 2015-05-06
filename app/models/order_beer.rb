88b7ec34bfaa6b7deebb4fbcc90421be21a43814
Kristina Brown <kristina.frey.frey@gmail.com>
2015-05-06 08:36:15 -0600

    logic taken out of the view

--- app/models/order_beer.rb
+++ app/models/order_beer.rb
@@ -1,4 +1,12 @@
 class OrderBeer < ActiveRecord::Base
   belongs_to :beer
   belongs_to :order
+  
+  def self.quantities(beer_id, order_id)
+    where(order_id: order_id, beer_id: beer_id)
+  end
+  
+  def self.line_item_subtotal(beer_price, quantity)
+    (beer_price * quantity)/100
+  end
 end
