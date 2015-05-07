4c660e12be828ad0404175782cb613120d454296
ycartc <tracymcaruso@gmail.com>
2015-05-07 01:22:48 -0600

    HTML views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@
    @user = User.find_by(email: params[:session][:email])
     if @user != nil && @user.admin?
       session[:user_id] = @user.id
-      redirect_to admin_beers_path
+      redirect_to admin_dashboard_path
     elsif @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       if @cart.contents.empty?
