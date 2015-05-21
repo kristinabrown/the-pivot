b92218268fb459e2749da307489374ed83efef58
Kenny <chenkenn@gmail.com>
2015-05-21 11:18:19 -0600

    fixing dup additions to watchlist

--- app/controllers/watch_items_controller.rb
+++ app/controllers/watch_items_controller.rb
@@ -3,9 +3,13 @@ class WatchItemsController < ApplicationController
     item_id = params[:order][:item_id]
     item    = @cart.find_item(item_id)
     if item.available?
-      @cart.add_item(item)
-      session[:cart] = @cart.contents
-      render :index
+      if @cart.contents[item_id.to_i]
+        flash[:errors] = "The #{item.name} is unavailable."
+      else
+        @cart.add_item(item)
+        session[:cart] = @cart.contents
+        render :index
+      end
     else
       flash[:errors] = "The #{item.name} is unavailable."
       redirect_to stores_path
@@ -13,6 +17,7 @@ class WatchItemsController < ApplicationController
   end
 
   def destroy
+    byebug
     @cart.delete_item(params['item']['item_id'])
     render :index
   end
