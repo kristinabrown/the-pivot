89a167dbf0d2f993c4e18a7c54fb2f0a2ac4c4f1
Lori Culberson <bigskybandit@sbcglobal.net>
2015-05-21 12:00:35 -0600

    WIP user can click bid

--- spec/features/registered_user/user_can_view_bids_on_own_dashboard.rb
+++ spec/features/registered_user/user_can_view_bids_on_own_dashboard.rb
@@ -1,31 +1,62 @@
 require "rails_helper"
 
-RSpec.describe "registered user can bid" type: :feature do 
+RSpec.describe "unregistered user cannot bid", type: :feature do 
 
-  def create_user
-    user = User.create(fullname: "Sam Spade", 
-                       email: "sam@sample.com",
-                       role: 0,
-                       phone: "222-333-4444",
-                       password: "password",
-                       street: "123 First Ave",
-                       city: "Denver",
-                       state: "CO",
-                       zipcode: "80211",
-                       credit_card: "4242424242424242",
-                       cc_expiration_date: "11/2015"
-                      )
+  before(:each) do 
+    store = Store.create!(name: "Collectibles Store")
+
+    category = Category.create!(name: "Automobiles")
+
+    item =  Item.create!(name: "moon car", description: "rocky",
+                          expiration_date: "Time.now + 10.days",
+                          starting_price: 10,
+                          active: true, 
+                          category_id: category.id, 
+                          store_id: store.id )
+
+    user = User.create!(fullname: "Sam Spade", 
+                         email: "sam@sample.com",
+                         display_name: "Sammie",
+                         role: 0,
+                         phone: "222-333-4444",
+                         password: "password",
+                         street: "123 First Ave",
+                         city: "Denver",
+                         state: "CO",
+                         zipcode: "80211",
+                         credit_card: "4242424242424242",
+                         cc_expiration_date: "2015-11-05"
+                        )
   end
 
-  def user_logs_in
-    visit login_path
-    fill_in("user[email]",    with: "sam@example.com" )
-    fill_in("user[password]", with: "password" )  
-    click_button "Login"
-  end
-  
   it "can see items to bid on" do 
-    user_logs_in
-
+    visit stores_path
+    click_link "Collectibles Store"
+    click_link "moon car"
+    click_button "Bid Now"
+    expect(current_path).to eq(login_path)
   end
+
+  # it "can click on bid button and be routed to login" do
+
+  # end
+
+
+
+
 end
+
+
+  # def create_user
+ 
+    # user_logs_in
+    # expect(current_path).to eq(users_path)
+    # expect(page).to have_content("Welcome, #{user.display_name}!")
+  # end
+
+  # def user_logs_in
+  #   visit login_path
+  #   fill_in("session[email]",    with: "sam@sample.com" )
+  #   fill_in("session[password]", with: "password" )  
+  #   click_button "Login"
+  # end
