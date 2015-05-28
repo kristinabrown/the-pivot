803ff104805f9f09cb01b7a3f6167de32605b294
Kristina Brown <kristina.frey.frey@gmail.com>
2015-05-28 12:20:15 -0600

    email when you win

---
+++ app/mailers/bid_mailer.rb
@@ -0,0 +1,9 @@
+class BidMailer < ApplicationMailer
+  
+  def winning_email(user, order)
+    @user = user
+    @order = order
+    
+    mail(to: @user.email, subject: "Order confirmation for #{@order.item.name}")
+  end
+end
